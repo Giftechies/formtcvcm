59bb92657f3d00c4b024413af960d3ec4cc8851f
Giftechies <info@giftechies.com>
2025-10-10 14:04:55 +0530

    update

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -385,7 +385,7 @@ export const getAllUsers = async (req, res) => {
       },
     ]);
 
-    res.status(200).json([]);
+    res.status(200).json(users);
   } catch (error) {
     console.error("Error fetching all users:", error);
     res.status(500).json({ message: "Server error" });
