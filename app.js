70d5e0a41aa26a393b196cba273307d8eae3e4ac
404manveer <manveershah007@gmail.com>
2025-12-18 13:56:23 +0530

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: ["https://formulaires-tcvcm.ca", "http://localhost:5500","https://tcvcm.ca/"],
+    origin: ["https://formulaires-tcvcm.ca", "http://localhost:5500","https://tcvcm.ca/",'tcvcm.ca'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
